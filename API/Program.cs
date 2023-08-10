c81c707b0b07e19765b13541343e046917876ec6
Roman Portianko <rportyanko10@gmail.com>
2023-08-10 18:25:17 +0300

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
